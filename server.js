e6eb0f576c4b39a0abc678c8d9f4935c0e18de6f
maigre <thomas@37m.gr>
2024-03-19 17:54:14 +0100

    working flow proto mardi3

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { Server as HttpServer } from 'http';
 import { Server as IoServer } from "socket.io";
-import multer from 'multer';
 import fs from 'fs';
 import { JSONFilePreset } from 'lowdb/node'
 import _        from 'lodash'
 import fetch from 'node-fetch';
+import crypto from 'crypto';
 
 // Path
 import path from 'path'
@@ -22,27 +22,19 @@ var app = express();
 var server = HttpServer(app);
 var io = new IoServer(server);
 
-// Storage
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/')
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + '.jpg') //Appending .jpg
-    }
-})
-var upload = multer({ storage: storage })
-
 
 // Request AI
 //
 var requestAI = async function(reqid) {
     var request = db.data.requests.find((req) => req.uuid === reqid)
     if (request) {
-        var reqdata = {input: {a: 1}}
+        var raw = JSON.stringify(request.input)
 
         // Call POST request to API
-        const response = await fetch('https://httpbin.org/post', {method: 'POST', body: JSON.stringify(reqdata) });
+        console.log(raw)
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        const response = await fetch('http://localhost:5000/predictions', {method: 'POST', headers: myHeaders, body: raw, redirect: "follow"})
         const data = await response.json();
         return data
     }
@@ -63,6 +55,9 @@ io.on('connection', (socket) =>
   // Client is ready, tell him what to do
   socket.on('hi', (userid) => 
   {
+    // join userid room
+    socket.join(userid)
+
     // check if nickname is set
     var user = db.data.users.find((user) => user.userid === userid) 
     if (user) socket.emit('nickname', user.nick)
@@ -70,20 +65,6 @@ io.on('connection', (socket) =>
     // check if a request is pending
     var request = db.data.requests.filter((req) => req.userid === userid)
     if (request) socket.emit('generate', request)
-  })
-
-  socket.on('retry', (reqid) => {
-    console.log('retry', reqid)
-    var request = db.data.requests.find((req) => req.uuid === reqid)
-    requestAI(reqid).then((res) => 
-    {
-        console.log('request processed', res)
-        request.status = "done"
-        db.write()
-        console.log('done', reqid)
-        socket.emit('reload')
-    })
-
   })
   
   // Send initial HELLO trigger
@@ -99,29 +80,107 @@ server.listen(4000, function() {
   console.log('listening on *:4000');
 });
 
-app.post('/generate',  upload.single('selfie'), function(req, res) {
+app.use(express.json({limit: '50mb'}));
 
-    // Get the data
-    var data = req.body;
-    var selfie = req.file;
 
-    // Save nick if not exists for uuid
-    var user = db.data.users.find((user) => user.userid === data.userid)
-    if (!user) {
-        db.data.users.push({userid: data.userid, nick: data.nick})
-        db.write()
+app.post('/gen', function(req, res) {
+
+  // Get JSON
+  var data = req.body;
+  console.log("=========== /gen REQUEST  =========");
+  console.log(data);      // your JSON
+
+  // Save nick if not exists for uuid
+  var user = db.data.users.find((user) => user.userid === data.userid)
+  if (!user) {
+      db.data.users.push({userid: data.userid, nick: data.nick})
+      db.write()
+      console.log('==== new user nickname saved', data.userid)
+  }
+
+  // extract selfie base64 and save to uploads/
+  var selfie = data.selfie;
+  var selfieData = selfie.replace(/^data:image\/\w+;base64,/, "");
+  var buf = Buffer.from(selfieData, 'base64');
+  var filename = 'uploads/'+data.userid+'_'+Date.now()+'.jpg';
+  fs.writeFileSync(filename, buf);
+  data.selfie = filename;
+
+  // select model image and prompt
+  // TODO: get modelname from user inputs
+  var modelname = 'PIG_00012_'
+
+  // get prompt from modelname.txt
+  // if file does not exists, create it
+  if (!fs.existsSync('models/'+modelname+'.txt'))
+    fs.writeFileSync('models/'+modelname+'.txt', 'a person')
+  var prompt = fs.readFileSync('models/'+modelname+'.txt', 'utf8');
+
+  // New AI request
+  var reqid = 'req_'+Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+  var inreq = {
+    "input": {
+      "image": "https://contacts.kxkm.net/"+filename,
+      "image_to_become": "https://contacts.kxkm.net/models/"+modelname+".png",
+      "prompt": prompt,
+      "negative_prompt": "",
+      "number_of_images": 1,
+      "denoising_strength": 1,
+      "prompt_strength": 0.8,
+      "control_depth_strength": 0.3,
+      "instant_id_strength": 0.5,
+      "image_to_become_strength": 0.8,
+      "image_to_become_noise": 0.53,
+      "seed": 9,  // 194246477, // crypto.randomBytes(4).readUInt32BE(0, true)
+      "disable_safety_checker": true
     }
+  }
+  var request = {uuid: reqid, userid: data.userid, data: data, input: inreq, output: [], status: "pending"};
 
-    // New request
-    var uuid = 'req_'+Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
-    var request = {uuid: uuid, userid: data.userid, data: data, selfie: selfie, status: "pending"};
+  // Save the request to DB
+  db.data.requests.push(request)
+  db.write()
 
-    // Save the request
-    db.data.requests.push(request)
-    db.write()
+  // Query AI
+  console.log('=== request AI', reqid)
+  requestAI(reqid).then((res) => 
+  {
+      console.log('=== request processed', reqid)
+      console.log(res)
 
-    // res.sendFile(__dirname + '/www/app.html');
-    res.redirect('/');
+      // pick the request
+      var request = db.data.requests.find((req) => req.uuid === reqid)
+
+      // detect error
+      if (res.error || !res.output) {
+        console.log('\tERROR: No output')
+        console.log('\t', res.error)
+        request.status = "error"
+        db.write()
+        io.to(request.userid).emit('error', request, res)
+        return
+      }
+
+      var request = db.data.requests.find((req) => req.uuid === reqid)
+
+      // Save base64 result to outputs/
+      for (var i=0; i<res.output.length; i++) {
+        var out = res.output[i];
+        var outData = out.replace(/^data:image\/\w+;base64,/, "");
+        var buf = Buffer.from(outData, 'base64');
+        var filename = 'outputs/'+reqid+'_'+i+'.jpg';
+        fs.writeFileSync(filename, buf);
+        request.output.push(filename);
+      }
+
+      request.status = "done"
+      db.write()
+      console.log('done', reqid)
+      io.to(request.userid).emit('done', request)
+  })
+
+  // Echo request 
+  res.send(request);
 });
 
 app.get('/totem', function(req, res) {
@@ -133,7 +192,10 @@ app.get('/', function(req, res) {
 });
 
 
+
 // Serve static files /static
 app.use('/static', express.static('www'));
 app.use('/uploads', express.static('uploads'));
+app.use('/models', express.static('models'));
+app.use('/outputs', express.static('outputs'));
 
--- www/app.js
+++ www/app.js
@@ -2,9 +2,53 @@
 // UUID
 //
 var uuid = Cookies.get('userid') || Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
-Cookies.set('userid', uuid)
+Cookies.set('userid', uuid, { expires: 3700 })
 $('#userid').val(uuid)
 console.log(uuid)
+
+// Generate BUTTON
+//
+$('#goBtn').click(function() 
+{
+    // Capture image from webcam as base64
+    var canvas = document.createElement('canvas');
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height); 
+    var dataURL = canvas.toDataURL('image/jpg');
+
+    // Build the data
+    var data = {
+        userid: uuid,
+        nick: $('#nick').val(),
+        hybrid: $('#hybrid').val(),
+        selfie:  dataURL
+    }
+
+    // Hide/Show Parts
+    $('.part').hide()
+
+    // Fetch POST /gen => Create pending request on server 
+    fetch('/gen', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    })
+    .then(response => {
+        // Request created (pending)
+        console.log("== New request created:")
+        console.log(response)
+        $('#reqid').html(response.uuid)
+        $('#progress').show()
+    })
+    .catch(error => {
+
+        // Request creation error..
+        console.error('== Request error:', error)
+        $('#reqerr').html(error)
+        $('#error').show()
+    })
+})
 
 // Socket.io
 //
@@ -19,42 +63,45 @@ socket.on('nickname', function(nick) {
     $('#nick').attr('disabled', 'disabled')    
 })
 
-socket.on('generate', function(requests) {
-
-    // not array return
-    if (!requests) return
-
-    var showNew = true
-    var showDone = true
-    
-    for (var req of requests) 
-    {
-        if (req.status === "done") 
-        {
-            $('<img>').attr('src', '/'+req.selfie.path).appendTo('#done')
-        } 
-        else if (req.status === "pending") 
-        {
-            $('#generate').show()
-            $('#reqid').html(req.uuid)
-            showNew = false
-            showDone = false
-        }
-    }
-
-    if (showNew) $('#new').show()
-    if (showDone) $('#done').show()
+socket.on('error', function(req, err) {
+    console.error('== AI error:', err)
+    $('.part').hide()
+    $('#error').show()
+    $('#reqerr').html(
+        JSON.stringify(req) + '<br><br>' +
+        JSON.stringify(err))
 })
+
+socket.on('done', function(req) {
+    console.log('done', req)
+    $('.part').hide()
+    $('#done').show()
+    $('<img>').attr('src', req.output).appendTo('#done')
+    // $('<div class="request">').html(JSON.stringify(req)).appendTo('#done')
+})
+
 
 socket.on('reload', function() {
     location.reload()
 })
 
 
-// Retry Generate
-$('#reqid').click(function() {
-    console.log('retry', this.innerHTML)
-    socket.emit('retry', this.innerHTML)
+// Retry button
+//
+$('#retryBtn').click(function() {
+    location.reload()
 })
 
 
+// Webcam
+//
+var video = document.querySelector("#selfieElement");
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+      video.srcObject = stream;
+    })
+    .catch(function (err0r) {
+      console.log("Something went wrong!");
+    });
+}
